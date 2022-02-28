5ca345292fd6eec6c00a7af0681c9cbafe44516d
khutjomaputla <khutjomaputla@gmail.com>
2022-02-28 22:54:37 +0200

    json object test

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get('/', function(req, res) {
   });
 
 app.post('/posttest', function(req, res) {
+  let person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age     : 50,
+    eyeColor  : "blue"
+  };
+  let output = JSON.stringify(person)
+  res.send(output)
+});
+
+app.post('/posttest', function(req, res) {
   res.send('i got this data ======> '+ req.body + '<======')
 });
   
